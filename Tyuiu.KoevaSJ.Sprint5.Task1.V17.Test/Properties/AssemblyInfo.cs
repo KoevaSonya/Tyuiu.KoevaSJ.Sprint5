bd33016a5bcb9cdee5359f5f7b3df057af13001d
Tyuiu.KoevaSJ@outlook.com <koevasonya_0902@mail.ru>
2023-11-19 19:58:49 +0500

    Task1

---
+++ Tyuiu.KoevaSJ.Sprint5.Task1.V17.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KoevaSJ.Sprint5.Task1.V17.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KoevaSJ.Sprint5.Task1.V17.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5592e853-c092-4e56-b1f1-11b4f0cd003b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KoevaSJ.Sprint5.Task1.V17/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+using Tyuiu.KoevaSJ.Sprint5.Task1.V17.Lib;
+
+namespace Tyuiu.KoevaSJ.Sprint5.Task1.V17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startValue = 5;
+            int stopValue = -5;
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила : Коева С. Ю. | АСОиУб-23-1";
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                    *");
+            Console.WriteLine("* Класс File. Запись набора данных в текстовый файл                            *");
+            Console.WriteLine("* Задание #1                                                                   *");
+            Console.WriteLine("* Вариант #17                                                                  *");
+            Console.WriteLine("* Выполнила Коева Софья Юрьевна | АСОиУб-23-1                                  *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                     *");
+            Console.WriteLine("* Дана функция, произвести табулирование от -5 до 5 с шагом 1. Произвести      *");
+            Console.WriteLine("* проверку деления на ноль. При делении на ноль вернуть значение 0.            *");
+
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                             *");
+            Console.WriteLine("********************************************************************************");
+
+            Console.WriteLine("startValue = " + startValue);
+            Console.WriteLine("stopValue = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(startValue, stopValue);
+
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
