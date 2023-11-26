851a397f02642fae08ef5f828751e83a1bc138a4
Tyuiu.KoevaSJ@outlook.com <koevasonya_0902@mail.ru>
2023-11-26 20:19:07 +0500

    Task5

---
+++ Tyuiu.KoevaSJ.Sprint5.Task5.V20.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KoevaSJ.Sprint5.Task5.V20.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KoevaSJ.Sprint5.Task5.V20.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ec1c0c9d-f429-44fc-b653-3c7b322e1406")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KoevaSJ.Sprint5.Task5.V20/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using Tyuiu.KoevaSJ.Sprint5.Task5.V20.Lib;
+
+namespace Tyuiu.KoevaSJ.Sprint5.Task5.V20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнила : Коева С. Ю. | АСОиУб-23-1";
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                    *");
+            Console.WriteLine("* Обработка файлов                                                             *");
+            Console.WriteLine("* Задание #5                                                                   *");
+            Console.WriteLine("* Вариант #20                                                                  *");
+            Console.WriteLine("* Выполнила Коева Софья Юрьевна | АСОиУб-23-1                                  *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                     *");
+            Console.WriteLine("*  Найти среднее значение всех целых чисел в файле,                            *");
+            Console.WriteLine("* которые находятся в промежутке от -10 до 10.                                 *");
+            Console.WriteLine("*                                                                              *");
+            Console.WriteLine("********************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask5V20.txt";
+            Console.WriteLine("Файл: " + path);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+            Console.WriteLine("Среднее значение = " + res);
+            Console.ReadKey();
+        }
+    }
+}
